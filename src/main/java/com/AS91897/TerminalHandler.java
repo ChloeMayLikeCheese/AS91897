dfa65db2c361be18085ca8334266b949286956cb
chloe <cheeseidontlike4@gmail.com>
2025-05-28 12:27:29 +1200

    Added the ability to navigate up and down through the folders, also added sorting

--- src/main/java/com/AS91897/TerminalHandler.java
+++ src/main/java/com/AS91897/TerminalHandler.java
@@ -3,7 +3,6 @@ package com.AS91897;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
-
 import org.jline.keymap.BindingReader;
 import org.jline.keymap.KeyMap;
 import org.jline.terminal.Terminal;
@@ -22,16 +21,35 @@ public class TerminalHandler {
     }
 
     public TerminalHandler() throws IOException {
-
         while (true) {
             File curDir = new File(".");
-            getAllFiles(curDir);
+            File[] dirList = curDir.listFiles(new FilenameFilter() {
+                public boolean accept(File current, String name) {
+                    return new File(current, name).isDirectory();
+                }
+            });
+
+            File[] fileList = curDir.listFiles(new FilenameFilter() {
+                public boolean accept(File current, String name) {
+                    return new File(current, name).isFile();
+                }
+            });
+            File[] fileAndDirList = new File[dirList.length + fileList.length];
+
+            for (int i = 0; i < dirList.length; i++) {
+                fileAndDirList[i] = dirList[i];
+            }
+            for (int i = 0; i < fileList.length; i++) {
+                fileAndDirList[dirList.length + i] = fileList[i];
+            }
+            
+            int selectedIndex = 0;
+            
             try (Terminal terminal = TerminalBuilder.builder()
                     .name("File Manager")
                     .jna(true)
                     .build()) {
                 terminal.enterRawMode();
-
 
                 BindingReader bindingReader = new BindingReader(terminal.reader());
                 KeyMap<Operation> keyMap = new KeyMap<>();
@@ -42,47 +60,63 @@ public class TerminalHandler {
                 keyMap.bind(Operation.ENTER, "\r", "\n");
                 keyMap.bind(Operation.COMMAND, "a", "b", "c", "d");
                 keyMap.bind(Operation.EXIT, "q");
-    
-                terminal.writer().println("Press arrow up/down/left/right, a/b/c/d, or 'q' to quit.");
-                terminal.writer().flush();
+
+                
                 boolean isReading = true;
                 while (isReading) {
+                    terminal.puts(Capability.clear_screen);
+                    terminal.writer().println("Directories:");
+                    for (int i = 0; i < fileAndDirList.length; i++) {
+                        if (i == fileList.length) {
+                            terminal.writer().println("Files:");
+                        }
+                        if (i == selectedIndex) {
+                            if (selectedIndex >= dirList.length) {
+                                terminal.writer().println(" > File: " + fileAndDirList[i].getName());
+                            }else{
+                                terminal.writer().println(" > Dir: " + fileAndDirList[i].getName());
+                            }
+                            
+                        } else {
+                            if (i >= dirList.length) {
+                                terminal.writer().println("  File: " + fileAndDirList[i].getName());
+                            }else{
+                                terminal.writer().println("  Dir: " + fileAndDirList[i].getName());
+                            }
+                        }
+                    }
+
+
+                    terminal.writer().println("Press arrow up/down/left/right, a/b/c/d, or 'q' to quit.");
+                    terminal.writer().flush();
+
                     Operation op = bindingReader.readBinding(keyMap, null, false);
                     if (op != null) {
-                        if (op == Operation.EXIT) {
-                            System.exit(0);
+                        switch (op) {
+                            case UP:
+                                if (selectedIndex > 0){
+                                    selectedIndex--;
+                                }
+                                    
+                                break;
+                            case DOWN:
+                                if (selectedIndex < fileAndDirList.length - 1){
+                                    selectedIndex++;
+                                }
+                                    
+                                break;
+                            case EXIT:
+                                isReading = false;
+                                break;
+                            default:
+
+                                break;
                         }
-                        terminal.puts(Capability.clear_screen);
-                        terminal.writer().println("read op: " + op);
-                        terminal.writer().flush();
-                        isReading = false;
                     }
 
                 }
-            }
-            
 
+            }
         }
-
     }
-    static void getAllFiles(File curDir) {
-
-        File[] filesList = curDir.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File current, String name){
-                return new File(current, name).isFile();
-            } 
-        });
-        File[] dirList = curDir.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File current, String name) {
-              return new File(current, name).isDirectory();
-            }
-        });
-        for (File d : dirList){System.out.println("Dir: "+d.getName());}
-        for (File f : filesList) {System.out.println("File: " + f.getName());}
-
-    }
-
-
 }
