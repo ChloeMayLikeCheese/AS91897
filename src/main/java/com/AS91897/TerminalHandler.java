3cefaadec5a9aae0614f30670f320e947bb192c1
chloe <cheeseidontlike4@gmail.com>
2025-06-09 20:27:33 +1200

    styilysed font clolor and hightlighed selected font

--- src/main/java/com/AS91897/TerminalHandler.java
+++ src/main/java/com/AS91897/TerminalHandler.java
@@ -5,11 +5,21 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import org.jline.keymap.BindingReader;
 import org.jline.keymap.KeyMap;
+import org.jline.terminal.Size;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import org.jline.terminal.Terminal.Signal;
+import org.jline.terminal.Terminal.SignalHandler;
 import org.jline.utils.InfoCmp.Capability;
 
 public class TerminalHandler {
+    Size terminalSize;
+    File curDir;
+    // File parentDir = curDir.getParentFile();
+    File[] dirList;
+    File[] fileList;
+    File[] fileAndDirList;
+
     enum Operation {
         UP,
         DOWN,
@@ -21,30 +31,12 @@ public class TerminalHandler {
     }
 
     public TerminalHandler() throws IOException {
+        curDir = new File("./").getAbsoluteFile();
+        updateFilesAndDirs();
         while (true) {
-            File curDir = new File(".");
-            File[] dirList = curDir.listFiles(new FilenameFilter() {
-                public boolean accept(File current, String name) {
-                    return new File(current, name).isDirectory();
-                }
-            });
-
-            File[] fileList = curDir.listFiles(new FilenameFilter() {
-                public boolean accept(File current, String name) {
-                    return new File(current, name).isFile();
-                }
-            });
-            File[] fileAndDirList = new File[dirList.length + fileList.length];
-
-            for (int i = 0; i < dirList.length; i++) {
-                fileAndDirList[i] = dirList[i];
-            }
-            for (int i = 0; i < fileList.length; i++) {
-                fileAndDirList[dirList.length + i] = fileList[i];
-            }
-            
+            // updateFilesAndDirs();
             int selectedIndex = 0;
-            
+
             try (Terminal terminal = TerminalBuilder.builder()
                     .name("File Manager")
                     .jna(true)
@@ -61,31 +53,31 @@ public class TerminalHandler {
                 keyMap.bind(Operation.COMMAND, "a", "b", "c", "d");
                 keyMap.bind(Operation.EXIT, "q");
 
-                
                 boolean isReading = true;
                 while (isReading) {
+                    int terminalWidth = terminal.getWidth();
+                    int terminalHeight = terminal.getHeight();
+
+                    terminalSize = terminal.getSize();
+
                     terminal.puts(Capability.clear_screen);
-                    terminal.writer().println("Directories:");
+                    terminal.writer().println(terminalWidth + "x" + terminalHeight);
+                    terminalSizeCallBack(terminal);
+
+                    // terminal.writer().println("Directories:");
                     for (int i = 0; i < fileAndDirList.length; i++) {
-                        if (i == fileList.length) {
-                            terminal.writer().println("Files:");
-                        }
+                        // if (i == fileList.length) {
+                        // terminal.writer().println("Files:");
+                        // }
                         if (i == selectedIndex) {
-                            if (selectedIndex >= dirList.length) {
-                                terminal.writer().println(" > File: " + fileAndDirList[i].getName());
-                            }else{
-                                terminal.writer().println(" > Dir: " + fileAndDirList[i].getName());
-                            }
-                            
+                            terminal.writer().print("\u001B[31m"+">"+"\u001B[0m"+"\u001B[47m");
+                        }
+                        if (i >= dirList.length) {
+                            terminal.writer().println("\u001B[33m"+fileAndDirList[i].getName()+"\u001B[0m");
                         } else {
-                            if (i >= dirList.length) {
-                                terminal.writer().println("  File: " + fileAndDirList[i].getName());
-                            }else{
-                                terminal.writer().println("  Dir: " + fileAndDirList[i].getName());
-                            }
+                            terminal.writer().println("\u001B[36m"+fileAndDirList[i].getName()+ "/ "+"\u001B[0m");
                         }
                     }
-
 
                     terminal.writer().println("Press arrow up/down/left/right, a/b/c/d, or 'q' to quit.");
                     terminal.writer().flush();
@@ -94,16 +86,21 @@ public class TerminalHandler {
                     if (op != null) {
                         switch (op) {
                             case UP:
-                                if (selectedIndex > 0){
+                                if (selectedIndex > 0) {
                                     selectedIndex--;
                                 }
-                                    
+
                                 break;
                             case DOWN:
-                                if (selectedIndex < fileAndDirList.length - 1){
+                                if (selectedIndex < fileAndDirList.length - 1) {
                                     selectedIndex++;
                                 }
-                                    
+
+                                break;
+                            case LEFT:
+                                curDir = curDir.getParentFile();
+                                updateFilesAndDirs();
+                                selectedIndex = 0;
                                 break;
                             case EXIT:
                                 isReading = false;
@@ -118,5 +115,39 @@ public class TerminalHandler {
 
             }
         }
+
+    }
+
+    public void setTerminalSizeVariable(Size terminalSize) {
+        this.terminalSize = terminalSize;
+
+    }
+
+    public void terminalSizeCallBack(Terminal terminal) {
+        terminal.puts(Capability.clear_screen);
+        terminal.writer().println("DEBUG: Screen cleared(Screen size update)");
+    }
+
+    public void updateFilesAndDirs() {
+        dirList = curDir.listFiles(new FilenameFilter() {
+            public boolean accept(File current, String name) {
+                return new File(current, name).isDirectory();
+            }
+        });
+
+        fileList = curDir.listFiles(new FilenameFilter() {
+            public boolean accept(File current, String name) {
+                return new File(current, name).isFile();
+            }
+        });
+        fileAndDirList = new File[dirList.length + fileList.length];
+
+        for (int i = 0; i < dirList.length; i++) {
+            fileAndDirList[i] = dirList[i];
+        }
+        for (int i = 0; i < fileList.length; i++) {
+            fileAndDirList[dirList.length + i] = fileList[i];
+        }
+
     }
 }
